ada2732cedb7cd9d28198ab78e78bd4eb221eb58
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-05-08 21:06:37 +0530

    basic data types

---
+++ 01_pr_basics_python/3/data_types.py
@@ -0,0 +1,13 @@
+# Strings
+a = 'abc' + "XYZ"
+print(type(a), a)
+
+# Numeric types
+b = 64
+c = 64.0
+d = 64 + 128j   # python uses 'j' for the imaginary part
+print(type(b), type(c), type(d))
+
+# Boolean
+e = 10 > 5
+print(d, type(d))
