886abd51255cf9c494f94a19b2fac02ce77afc7b
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-05-12 18:31:53 +0530

    section done

--- 01_pr_basics_python/4/loops.py
+++ 01_pr_basics_python/4/loops.py
@@ -1,1 +1,38 @@
 # 4.2: Loops and Iterations
+for counter in [1, 2, 3, 4, 5]:
+    print("This is a loop: counter = ", counter)
+
+# range()
+print(list(range(10)))
+print(list(range(5, 10)))
+print(list(range(0, 10, 2)))
+
+sum = 0
+for i in range(0, 101, 2):
+    sum += i
+print("Total of even numbers: ", sum)
+
+num, x = 100, []
+while num > 0:
+    x.append(num)
+    num -= 15
+print(x)
+
+# Nested loops
+for x in range(5):
+    for y in range(5):
+        print("$", end=" ")
+    print()
+
+# for-else example
+for letter in ['H', 'E', 'L', 'L', 'O']:
+    print(letter, end=" ")
+else:
+    print(", world!")
+
+for letter in ['H', 'E', 'L', 'L', 'O']:
+    print(letter, end=" ")
+    if letter == 'O':
+        break
+else:
+    print(" !")
