27b36abad73b06be8f204659ffb3450937c5f458
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-05-12 21:12:06 +0530

    io

--- 01_pr_basics_python/4/input.py
+++ 01_pr_basics_python/4/input.py
@@ -1,1 +1,10 @@
 # Input
+name = input("Enter your name: ")
+print(f"Hello, {name}!")
+
+# {} placeholders
+a, b = 3, 5
+print("Value of a is {} and value of b is {}".format(a,b))
+
+# {} placeholders with order specified
+print("Rank: {2}, First name: {1}, Last name: {0}".format('Potter', 'Harry', 'Wizard'))
