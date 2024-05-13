1ef0cfe7effcf20f1c2cee35195668fc2f7ff0d0
tharinducbd <tharinduchanaka@gmail.com>
2024-05-13 13:13:32 +0530

    section 4.4 content

--- 01_pr_basics_python/4/functions.py
+++ 01_pr_basics_python/4/functions.py
@@ -1,1 +1,31 @@
 # 4.4: Functions
+
+from math import pi
+
+# Function definition: def keyword, function name, parameters
+def print_me(msg: str) -> None:
+    # Docstring and the suit of code
+    """Print a capitalized message in terminal"""
+    print(msg.upper())
+    # The return statement. Not using an expression will return None.
+    return
+
+print_me("This is Python")
+
+# Exercise 1
+def greet(name: str) -> None:
+    """Print 'Hello!, name' in terminal"""
+    print(f"Hello!, {name}")
+    return
+
+greet("Monty Python")
+
+# Exercise 2
+def calc_circumference(r):
+    return 2 * pi * r
+
+def calc_area(r):
+    return pi * r**2
+
+print("{:.3f}".format(calc_circumference(10)))
+print("{:.3f}".format(calc_area(10)))
