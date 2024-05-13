1a3d1c21f2bcc9b2980648dae9d6a3fd2fd5fbea
tharinducbd <tharinduchanaka@gmail.com>
2024-05-13 13:35:27 +0530

    kw args, *args

--- 01_pr_basics_python/4/functions.py
+++ 01_pr_basics_python/4/functions.py
@@ -3,7 +3,7 @@
 from math import pi
 
 # Function definition: def keyword, function name, parameters
-def print_me(msg: str) -> None:
+def print_me(msg: str='Hi!') -> None:
     # Docstring and the suit of code
     """Print a capitalized message in terminal"""
     print(msg.upper())
@@ -11,6 +11,8 @@ def print_me(msg: str) -> None:
     return
 
 print_me("This is Python")
+print_me()
+print(print_me.__doc__)
 
 # Exercise 1
 def greet(name: str) -> None:
@@ -28,4 +30,15 @@ def calc_area(r):
     return pi * r**2
 
 print("{:.3f}".format(calc_circumference(10)))
-print("{:.3f}".format(calc_area(10)))
+print("{:.3f}".format(calc_area(r=10)))
+
+# Arbitrary arguments
+def log(*args):
+    print("-----", end="")
+    for entry in args:
+        print(entry, end="-----")
+    print()
+    return
+
+log("Life", "Universe", "and", "Everything")
+
