372566dabbb90e1ecfd0ad9cafaed822aee00f13
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-05-13 21:03:33 +0530

    open() close() read() readline() readlines()

--- 01_pr_basics_python/5_python_looking_ahead/file.py
+++ 01_pr_basics_python/5_python_looking_ahead/file.py
@@ -1,1 +1,32 @@
 # Files
+
+# Opening and closing a file
+file_handle = open("lorem_ipsum.txt", "r") # 'r' is optional as it is the default
+file_handle.close()
+
+# Reading from a file: read()   >> read entire file as a string
+file_handle = open("lorem_ipsum.txt", "r")
+file_contents = file_handle.read()
+file_handle.close()
+# print(file_contents)
+
+# Reading from a file: readline()    >> read one line at a time.
+file_handle = open("lorem_ipsum.txt", "r")
+file_contents = file_handle.readline()
+print(file_contents)
+file_contents = file_handle.readline()
+print(file_contents)
+file_handle.close()
+
+# Reading from a file: readlines()   >> return a list of lines
+file_handle = open("lorem_ipsum.txt", "r")
+file_contents = file_handle.readlines()
+for line in file_contents:
+    print(line)
+file_handle.close()
+
+# Writing to a file: write(string)
+file_handle = open("lorem_ipsum_2", "w")
+my_string = "Python is excellent.\nAnd File handline is so easy.\nAnd everything else!"
+file_handle.write(my_string)
+file_handle.close()
