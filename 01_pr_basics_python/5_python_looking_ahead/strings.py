dbcbe6886988be05bdcdbdc2cef234dbf130b189
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-05-14 20:55:03 +0530

    strings: concatenation and slicing

---
+++ 01_pr_basics_python/5_python_looking_ahead/strings.py
@@ -0,0 +1,13 @@
+# Strings
+word = "Hello " 'world!'    # 'Nearby concatenation'
+print(word)
+
+word = "Life, Universe, and Everything"
+print(word[6])
+print(word[6:])
+print(word[6:14])
+
+print(word[-1::-1])
+print(word[-10:])
+
+print(word[:-16])
